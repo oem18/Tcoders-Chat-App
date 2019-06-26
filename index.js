8b95105a52a06100cc68de180b4950889681eb6a
Stanliwie <stanliwise@gmail.com>
2019-06-26 23:05:59 +0100

    made changes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res)=>{
-    req.send("hello, world");
-})
+app.get("/", (req, res)=>res.send("hello, world"));
 
 app.listen(port, ()=> console.log(`chat app listening on port ${port}`));
